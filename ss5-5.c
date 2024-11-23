974377ec39cec1f00ddbbd99ec45c831d7237f7e
ThanhDatThanhDat <dt2229992@gmail.com>
2024-11-23 23:47:02 +0700

    ss5-5

---
+++ ss5-5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i; 
+    for ( i = 1; i <= 9; i++) {
+        printf("Bang cuu chuong %d:\n", i);
+        
+        int j; 
+        for ( j = 1; j <= 9; j++) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
+
